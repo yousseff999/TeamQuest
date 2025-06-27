69e4f2413685345e756db23e6edd7a53d68a4daf
youssefzammit <youssef.zammit@esprit.tn>
2025-06-27 23:03:16 +0100

    dd

--- Desktop/TeamQuest-master/TeamQuest-master/src/main/java/org/example/config/SecurityConfig.java
+++ Desktop/TeamQuest-master/TeamQuest-master/src/main/java/org/example/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -90,6 +91,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Allow Angular frontend
+        configuration.setAllowedOrigins(List.of("http://192.168.56.10:4200"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); // Allow credentials (cookies)
