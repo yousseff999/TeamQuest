fb59c0491cbc5a70dcb8f4b2daf3b75e44e030ae
yousseff999 <115430423+yousseff999@users.noreply.github.com>
2025-06-28 16:58:12 +0100

    Update environment.ts

--- Desktop/TeamQuestAng-main/TeamQuestAng-main/src/environments/environment.ts
+++ Desktop/TeamQuestAng-main/TeamQuestAng-main/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://192.168.56.10:8086/api/auth',
-  apiBaseUrl: 'http://192.168.56.10:8086' // or your Docker host IP
-
+  //apiBaseUrl: 'http://192.168.56.10:8086' // or your Docker host IP
+apiBaseUrl: 'localhost:8086' 
 };
